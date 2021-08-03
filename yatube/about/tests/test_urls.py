5758fc0beee23834f22c8b7263db10a6248b48f5
Batalova Anastasiya <batalova.av@gmail.com>
2021-08-03 22:47:38 +0300

    faild tests

--- yatube/about/tests/test_urls.py
+++ yatube/about/tests/test_urls.py
@@ -11,5 +11,5 @@ class StaticURLTests(TestCase):
         urls = ['/about/author/', '/about/tech/']
         for url_name in urls:
             with self.subTest(url_name=url_name):
-                response = self.guest_client.get(reverse(url_name))
+                response = self.guest_client.get(url_name)
                 self.assertEqual(response.status_code, 200)
